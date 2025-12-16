ed8537ae20a80d84d38f7d64c42d6af8aef3f971
pdr0w0 <pdrowo05@gmail.com>
2025-12-16 14:14:57 +0100

    Sistema de posionado de torres básico
    Añadido el sistema básico de posicionado de torres.
    UI botón test para colocar torres
    Cambiado el prefab del enemigo base para poder visualizar mejor su rotación

---
+++ Classes/Player/TowerPlacing.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class TowerPlacing : MonoBehaviour
+{
+    [SerializeField] private LayerMask PlacementCheckMask;
+    [SerializeField] private LayerMask PlacementCollideMask;
+    [SerializeField] private Camera PlayerCamera;
+
+    private GameObject CurrentPlacingTower;
+
+
+
+    void Start()
+    {
+
+    }
+
+
+ void Update()
+    {
+        if(CurrentPlacingTower != null)
+        {
+            Ray camray = PlayerCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit HitInfo;
+            if (Physics.Raycast(camray, out HitInfo, 100f, PlacementCollideMask))
+            {
+                CurrentPlacingTower.transform.position = HitInfo.point;
+            }
+
+            if(Input.GetMouseButtonDown(0) && HitInfo.collider.gameObject != null)
+            {
+                if(!HitInfo.collider.gameObject.CompareTag("NoPlace"))
+                {
+                    BoxCollider TowerCollider = CurrentPlacingTower.GetComponent<BoxCollider>();
+                    TowerCollider.isTrigger = true;
+
+                    Vector3 BoxCenter = CurrentPlacingTower.gameObject.transform.position + TowerCollider.center;
+                    Vector3 HalfExtents = TowerCollider.size / 2;
+                    if (!Physics.CheckBox(BoxCenter, HalfExtents, Quaternion.identity, PlacementCheckMask, QueryTriggerInteraction.Ignore))
+                    {
+                        TowerCollider.isTrigger = false;
+                        CurrentPlacingTower = null;
+                        
+                    }
+                    
+
+                }
+                
+            }
+        }
+    }
+
+    public void SetTowerToPlace(GameObject tower)
+    {
+        CurrentPlacingTower = Instantiate(tower, Vector3.zero, Quaternion.identity);
+    }
+
+
+}
